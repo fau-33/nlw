37ac175383108315ef3293229e5ceff80fc6ade8
Flávio Félix <fau_felix30@hotmail.com>
2021-06-26 06:28:05 -0300

    Concluído a aula 04

--- main.js
+++ main.js
@@ -18,18 +18,18 @@ for(const link of links){
 }
 
 /** Mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
-  if(window.scroll >= navHeight){
+  if(window.scrollY >= navHeight){
     // scroll é maior que a altura do header
     header.classList.add('scroll')
   }else{
     // scroll é menor que a altura
     header.classList.remove('scroll')
   }
-})
+}
 
 /** Testimonials carousel slider swipper */
 const swiper = new Swiper('.swiper-container', {
@@ -54,6 +54,23 @@ scrollReveal.reveal(`
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
 `,{interval: 100})
- 
+
+/** Botão voltado para o topo */
+function backToTop(){
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(window.scrollY >= 560){
+    backToTopButton.classList.add('show')
+  }else{
+    backToTopButton.classList.remove('show')
+  }
+}
+/** When Scroll */
+window.addEventListener('scroll', function(){
+  changeHeaderWhenScroll()
+  backToTop()
+})
+
